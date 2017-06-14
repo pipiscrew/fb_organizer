b778e2a938c32f4a752c26f4f4251f2aed7425f6
pipiscrew <pipiscrew@gmail.com>
2017-06-14 20:25:55 +0200

    Create index.php

---
+++ jquery_load/index.php
@@ -0,0 +1,18 @@
+<button id="btn_ad_fees_new" type="button" class="btn btn-primary">	New</button>
+
+<div id="x_details">
+</div>
+
+<script>
+		$('#btn_x_new').on('click', function(e) {
+			$("#loading").height($('body').height());
+			$("#loading").show();
+
+			$("#x_details").load('x_details.php', function() {
+				$("#loading").hide();
+				$("#x").hide(); //the grid div
+				
+				$("#x_details").show(); //the table add new elements
+			});
+		});
+</script>
